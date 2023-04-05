7604eb2757f5284689ddba8b8b0c31c8ff365ace
Jaron Li <2002jaron@gmail.com>
2023-04-05 16:56:44 -0700

    fixing

--- src/api/characters.py
+++ src/api/characters.py
@@ -39,6 +39,7 @@ def get_character(id: str):
             #   json["gender"] = null
             # else:
             #   json["gender"] = character["gender"]
+            print(json)
             return json
 
 
