a1f2e5c2d8bb3fdf5f531c53315655d4f19bfe70
Jaron Li <2002jaron@gmail.com>
2023-04-05 16:33:37 -0700

    fixing

--- src/api/characters.py
+++ src/api/characters.py
@@ -27,7 +27,7 @@ def get_character(id: str):
       originally queried character.
     """
     # json = None
-
+    found = False
     for character in db.characters:
         if character["character_id"] == id:
             print("character found")
@@ -39,10 +39,12 @@ def get_character(id: str):
               json["gender"] = null
             else:
               json["gender"] = character["gender"]
+            found = true
             break
-        else:
-          json = None
-
+        
+    if found is False:
+      json = None
+      
     if json is None:
         raise HTTPException(status_code=404, detail="movie not found.")
     
