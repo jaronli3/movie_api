70fd5c533eba738cad870e09d7c3658efe0f2930
Jaron Li <2002jaron@gmail.com>
2023-04-08 17:26:05 -0700

    trying get_char

--- src/api/characters.py
+++ src/api/characters.py
@@ -68,9 +68,9 @@ def get_character(id: str):
         if conversation["character1_id"] not in top_convo_dict:
           top_convo_dict["character1_id"] = 0
       # if char_found is True:
-      for line in db.lines:
-        if line["conversation_id"] == current_convo_id:
-          top_convo_dict[curr_char] = top_convo_dict[curr_char] + 1
+      #   for line in db.lines:
+      #     if line["conversation_id"] == current_convo_id:
+      #       top_convo_dict[curr_char] = top_convo_dict[curr_char] + 1
       
 
       # sorted_dict = sorted(top_convo_dict.items(), key=lambda x:x[1])
