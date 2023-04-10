03f52f0787b07c4dce4419319c5aee07941e9157
Jaron Li <2002jaron@gmail.com>
2023-04-10 12:02:41 -0700

    trying get_char

--- src/api/characters.py
+++ src/api/characters.py
@@ -86,7 +86,7 @@ def get_character(id: str):
     for x in sorted_top_convo_2:
       top_convo_3[x[0]] = x[1]
     # sorted_top_convo_2 = dict(sorted(top_convo_dict2.items(), key = lambda x:x[1], reverse = True))
-
+    json["top_convo"] = sorted_top_convo_2
     json["top_conversations"] = top_convo_3
 
 
