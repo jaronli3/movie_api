717d2cd50cdf986d283fe016024e38f0ed91dfe1
Jaron Li <2002jaron@gmail.com>
2023-04-10 19:17:29 -0700

    starting list_char

--- src/api/characters.py
+++ src/api/characters.py
@@ -142,14 +142,14 @@ def list_characters(
           dictionary = {}
           dictionary["character_id"] = character["character_id"]
           dictionary["character"] = character["name"]
-          for movie in db.movies:
-            if movie["movie_id"] == character["movie_id"]:
-              dictionary["movie"] = movie["title"]
-              movie_id = movie["movie_id"]
-          for line in db.lines:
-            if movie_id == line["movie_id"] and dictionary["character_id"] == line["character_id"]:
-              num_of_lines += 1
-          dictionary["number_of_lines"] = num_of_lines
+          # for movie in db.movies:
+          #   if movie["movie_id"] == character["movie_id"]:
+          #     dictionary["movie"] = movie["title"]
+          #     movie_id = movie["movie_id"]
+          # for line in db.lines:
+          #   if movie_id == line["movie_id"] and dictionary["character_id"] == line["character_id"]:
+          #     num_of_lines += 1
+          # dictionary["number_of_lines"] = num_of_lines
           json.append(dictionary)
 
     return json
