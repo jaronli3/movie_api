dbed8e27902826b70780b13dc65680760371a13b
Jaron Li <2002jaron@gmail.com>
2023-04-17 14:51:56 -0700

    started lines

--- src/api/lines.py
+++ src/api/lines.py
@@ -39,8 +39,8 @@ def get_line(line_id: str):
         #     character_1 = line["character_id"]
         #     break
         json = {}
-        json["line_id"] = line_id
-        json["line_text"] = line["line_text"]
+        # json["line_id"] = line_id
+        # json["line_text"] = line["line_text"]
         break
 
     if json is None:
