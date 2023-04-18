c373d1d90773644a1f253b7225dc3950beb794cb
Jaron Li <2002jaron@gmail.com>
2023-04-17 20:21:23 -0700

    started lines

--- src/api/lines.py
+++ src/api/lines.py
@@ -7,23 +7,22 @@ import operator
 
 router = APIRouter()
 
-# @router.get("/lines/{line_id}", tags=["lines"])
-# def get_line(line_id: str):
-#     """
-#     This endpoint returns a single line by its identifier. For each line it returns:
-#     * `line_id`: the internal id of the line.
-#     * `line_text`: The text of the line.
-#     * `characters_involved`: A list of characters that were involved in that conversation. 
+@router.get("/lines/{line_id}", tags=["lines"])
+def get_line(line_id: str):
+    """
+    This endpoint returns a single line by its identifier. For each line it returns:
+    * `line_id`: the internal id of the line.
+    * `line_text`: The text of the line.
+    * `characters_involved`: A list of characters that were involved in that conversation. 
 
-#     Each character is represented by a dictionary with the following keys:
-#     * `character_id`: the internal id of the character.
-#     * `character`: The name of the character.
-#     * `gender`: The gender of the character.
-#     """
-#     line = db.lines.get(line_id)
-#     print(line)
-#     if line:
-#         json = {"line_id": line.id, "line_text": line.line_text}
-#         return json
+    Each character is represented by a dictionary with the following keys:
+    * `character_id`: the internal id of the character.
+    * `character`: The name of the character.
+    * `gender`: The gender of the character.
+    """
+    line = db.lines.get(line_id)
+    if line:
+        json = {"line_id": line.id, "line_text": line.line_text}
+        return json
     
-#     raise HTTPException(status_code=404, detail="line not found.")
+    raise HTTPException(status_code=404, detail="line not found.")
