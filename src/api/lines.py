b970f199f3fe2a0d23f12bc97566ed26604d4422
Jaron Li <2002jaron@gmail.com>
2023-04-17 19:16:26 -0700

    started lines

--- src/api/lines.py
+++ src/api/lines.py
@@ -2,16 +2,15 @@ from fastapi import APIRouter, HTTPException
 from enum import Enum
 from src import database as db
 from fastapi.params import Query
-from collections import Counter
 
 import operator
 
 router = APIRouter()
 
 @router.get("/lines/{id}", tags=["lines"])
-def get_line(id: str):
+def get_line_by_character(id: str):
     """
-    This endpoint returns a single line by its identifier. For each line it returns:
+    This endpoint returns line(s) spoken by the character given through their character_id. For each line it returns:
     * `line_id`: the internal id of the line.
     * `line_text`: The text of the line.
     * 'movie': The movie the line came from.
@@ -24,106 +23,16 @@ def get_line(id: str):
     * 'age': The age of the character.
 
     """
-
+    
     json = None
-    movie_id = None
-    convo_id = None
-    character_1 = None
-    character_2 = None
+    
+    char = db.characters.get(id)
 
-    for line in db.lines:
-        if line["line_id"] == line_id:
-            json = {}
-            json["line_id"] = line_id
-            json["line_text"] = line["line_text"]
-            movie_id = line["movie_id"]
-            convo_id = line["conversation_id"]
-            character_1 = line["character_id"]
-            break
+    if char:
+        json = {}
+        json["movie"] = char["name"]
 
     if json is None:
         raise HTTPException(status_code=404, detail="line not found.")
-
-    # for movie in db.movies:
-    #     if movie_id == movie["movie_id"]:
-    #         json["movie"] = movie["title"]
     
-    # for convo in db.conversations:
-    #     if convo["character1_id"] == character_1:
-    #         character_2 = convo["character2_id"]
-    #     elif convo["character2_id"] == character_1:
-    #         character_2 = convo["character1_id"]
-
-
-    # lst = []
-    
-    # for char in db.characters:
-    #     char_json = {}
-    #     if char["character_id"] == character_1:
-    #         char_json["character_id"] = char["character_id"]
-    #         char_json["name"] = char["name"]
-    #         if character["gender"] == "":
-    #           char_json["gender"] = None
-    #         else:
-    #           char_json["gender"] = char["gender"]
-    #         char_json["age"] = int(char["age"])
-    #         lst.append(char_json)
-    #     elif char["character_id"] == character_2:
-    #         char_json["character_id"] = char["character_id"]
-    #         char_json["name"] = char["name"]
-    #         if character["gender"] == "":
-    #           char_json["gender"] = None
-    #         else:
-    #           char_json["gender"] = char["gender"]
-    #         char_json["age"] = int(char["age"])
-    #         lst.append(char_json)
-
-    # json["characters_involved"] = lst
-
     return json
-
-# Add get parameters
-# @router.get("/lines/", tags=["lines"])
-# def list_lines(
-#     line: str = "",
-#     limit: int = Query(50, ge=1, le=250),
-#     offset: int = Query(0, ge=0),
-# ):
-#     """
-#     This endpoint returns a list of lines. For each line it returns:
-#     * `line_id`: the internal id of the line. 
-#     * `line_text`: The text of the line.
-#     * `movie`: The movie the line came from.
-#     * 'characters_involved': A list of characters that were involved in that conversation. 
-
-#     Each character is represented by a dictionary with the following keys:
-#     * `character_id`: the internal id of the character.
-#     * `character`: The name of the character.
-#     * `gender`: The gender of the character.
-#     * 'age': The age of the character.
-
-#     The `limit` and `offset` query
-#     parameters are used for pagination. The `limit` query parameter specifies the
-#     maximum number of results to return. The `offset` query parameter specifies the
-#     number of results to skip before returning results.
-#     """
-#     json = []
-
-#     for movies in db.movies:
-#       if name.lower() in movies["title"]:
-#           dictionary = {}
-#           dictionary["movie_id"] = int(movies["movie_id"])
-#           dictionary["movie_title"] = movies["title"]
-#           dictionary["year"] = movies["year"]
-#           dictionary["imdb_rating"] = float(movies["imdb_rating"])
-#           dictionary["imdb_votes"] = int(movies["imdb_votes"])
-#           json.append(dictionary)
-
-#     if sort.lower() == "movie_title":
-#       return sorted(json, key=operator.itemgetter('movie_title'))[offset:limit + offset]
-#     elif sort.lower() == "year":
-#       return sorted(json, key=operator.itemgetter('year'))[offset:limit + offset]
-#     elif sort.lower() == "rating":
-#       return sorted(json, key=operator.itemgetter('imdb_rating'), reverse = True)[offset:limit + offset]
-
-#     return json[offset:limit + offset]
