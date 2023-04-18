eccc2b36e40e5a69dba85b3d75055791cb498491
Jaron Li <2002jaron@gmail.com>
2023-04-17 23:01:59 -0700

    working on get_char_lines

--- src/api/lines.py
+++ src/api/lines.py
@@ -77,9 +77,9 @@ def get_char_lines(character_id: int, limit: int = Query(50, ge=1, le=250), offs
                 dictionary = {}
                 dictionary["character_id"] = char.id
                 dictionary["character_name"] = char.name
-                movie = db.movies.get(line.line_id)
-                dictionary["movie_title"] = movie.title
-                dictionary["line"] = line.line_text
+                # movie = db.movies.get(line.line_id)
+                # dictionary["movie_title"] = movie.title
+                # dictionary["line"] = line.line_text
                 json.append(dictionary)
     return json
     # if sort.lower() == "movie_title":
