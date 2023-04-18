b4436837d315dcc41b13a1d0cb405dae6000d090
Jaron Li <2002jaron@gmail.com>
2023-04-17 20:08:30 -0700

    started lines

--- src/api/lines.py
+++ src/api/lines.py
@@ -6,21 +6,46 @@ from fastapi.params import Query
 import operator
 
 router = APIRouter()
+def get_top_conv_characters(character):
+    c_id = character.id
+    movie_id = character.movie_id
+    all_convs = filter(
+        lambda conv: conv.movie_id == movie_id
+        and (conv.c1_id == c_id or conv.c2_id == c_id),
+        db.conversations.values(),
+    )
+    line_counts = Counter()
 
-@router.get("/lines/{line_id}", tags=["lines"])
-def get_line(line_id: str):
+    for conv in all_convs:
+        other_id = conv.c2_id if conv.c1_id == c_id else conv.c1_id
+        line_counts[other_id] += conv.num_lines
+
+    return line_counts.most_common()
+
+
+@router.get("/lines/{id}", tags=["characters"])
+def get_character(id: int):
     """
-    This endpoint returns a single line by its identifier. For each line it returns:
-    * `line_id`: the internal id of the line.
-    * `line_text`: The text of the line.
-    * `characters_involved`: A list of characters that were involved in that conversation. 
+    This endpoint returns a single character by its identifier. For each character
+    it returns:
+    * `character_id`: the internal id of the character. Can be used to query the
+      `/characters/{character_id}` endpoint.
+    * `character`: The name of the character.
+    * `movie`: The movie the character is from.
+    * `gender`: The gender of the character.
+    * `top_conversations`: A list of characters that the character has the most
+      conversations with. The characters are listed in order of the number of
+      lines together. These conversations are described below.
 
-    Each character is represented by a dictionary with the following keys:
+    Each conversation is represented by a dictionary with the following keys:
     * `character_id`: the internal id of the character.
     * `character`: The name of the character.
     * `gender`: The gender of the character.
+    * `number_of_lines_together`: The number of lines the character has with the
+      originally queried character.
     """
-    character = db.characters.get(line_id)
+
+    character = db.characters.get(id)
 
     if character:
         movie = db.movies.get(character.movie_id)
@@ -42,6 +67,20 @@ def get_line(line_id: str):
         return result
 
     raise HTTPException(status_code=404, detail="character not found.")
+
+# @router.get("/lines/{line_id}", tags=["lines"])
+# def get_line(line_id: str):
+#     """
+#     This endpoint returns a single line by its identifier. For each line it returns:
+#     * `line_id`: the internal id of the line.
+#     * `line_text`: The text of the line.
+#     * `characters_involved`: A list of characters that were involved in that conversation. 
+
+#     Each character is represented by a dictionary with the following keys:
+#     * `character_id`: the internal id of the character.
+#     * `character`: The name of the character.
+#     * `gender`: The gender of the character.
+#     """
     # line = db.lines.get(line_id)
     # print(line)
     # if line:
