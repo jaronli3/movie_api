850e119adddb39c1b2d8dc64f44f5df7db06e395
Jaron Li <2002jaron@gmail.com>
2023-04-17 21:00:32 -0700

    started lines

--- src/api/lines.py
+++ src/api/lines.py
@@ -46,29 +46,29 @@ def get_line(line_id: int):
                     char2 = convo["character1_id"]
                 break
                 
-        lst = []
-        for char in db.characters:
-            char_json = {}
-            if char["character_id"] == char1:
-                char_json["character_id"] = char["character_id"]
-                char_json["name"] = char["name"]
-                if character["gender"] == "":
-                    char_json["gender"] = None
-                else:
-                    char_json["gender"] = char["gender"]
-                char_json["age"] = int(char["age"])
-                lst.append(char_json)
-            elif char["character_id"] == char2:
-                char_json["character_id"] = char["character_id"]
-                char_json["name"] = char["name"]
-                if character["gender"] == "":
-                    char_json["gender"] = None
-                else:
-                    char_json["gender"] = char["gender"]
-                char_json["age"] = int(char["age"])
-                lst.append(char_json)
-                
-        json["characters involved"] = lst
+        # lst = []
+        # for char in db.characters:
+        #     char_json = {}
+        #     if char["character_id"] == char1:
+        #         char_json["character_id"] = char["character_id"]
+        #         char_json["name"] = char["name"]
+        #         if character["gender"] == "":
+        #             char_json["gender"] = None
+        #         else:
+        #             char_json["gender"] = char["gender"]
+        #         char_json["age"] = int(char["age"])
+        #         lst.append(char_json)
+        #     elif char["character_id"] == char2:
+        #         char_json["character_id"] = char["character_id"]
+        #         char_json["name"] = char["name"]
+        #         if character["gender"] == "":
+        #             char_json["gender"] = None
+        #         else:
+        #             char_json["gender"] = char["gender"]
+        #         char_json["age"] = int(char["age"])
+        #         lst.append(char_json)
+
+        # json["characters involved"] = lst
 
         return json
     
