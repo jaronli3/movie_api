8b1d74915277d4a2d00b37572e5c075a9b5b3a98
Jaron Li <2002jaron@gmail.com>
2023-04-17 19:50:11 -0700

    started lines

--- src/api/movies.py
+++ src/api/movies.py
@@ -8,7 +8,7 @@ import operator
 router = APIRouter()
 
 
-@router.get("/lines/{movie_id}", tags=["movies"])
+@router.get("/movies/{movie_id}", tags=["movies"])
 def get_movie(movie_id: int):
     """
     This endpoint returns a single movie by its identifier. For each movie it returns:
@@ -17,39 +17,28 @@ def get_movie(movie_id: int):
     * `top_characters`: A list of characters that are in the movie. The characters
       are ordered by the number of lines they have in the movie. The top five
       characters are listed.
-
     Each character is represented by a dictionary with the following keys:
     * `character_id`: the internal id of the character.
     * `character`: The name of the character.
     * `num_lines`: The number of lines the character has in the movie.
-
     """
 
-    # movie = db.movies.get(movie_id)
-    # if movie:
-    #     top_chars = [
-    #         {"character_id": c.id, "character": c.name, "num_lines": c.num_lines}
-    #         for c in db.characters.values()
-    #         if c.movie_id == movie_id
-    #     ]
-    #     top_chars.sort(key=lambda c: c["num_lines"], reverse=True)
+    movie = db.movies.get(movie_id)
+    if movie:
+        top_chars = [
+            {"character_id": c.id, "character": c.name, "num_lines": c.num_lines}
+            for c in db.characters.values()
+            if c.movie_id == movie_id
+        ]
+        top_chars.sort(key=lambda c: c["num_lines"], reverse=True)
 
-    #     result = {
-    #         "movie_id": movie_id,
-    #         "title": movie.title,
-    #         "top_characters": top_chars[0:5],
-    #     }
-    #     return result
-    for line in db.lines:
-        if line["line_id"] == movie_id:
-            json = {}
-            json["line_id"] = line["line_id"]
-            json["line_text"] = line["line_text"]
-            movie_id = line["movie_id"]
-            convo_id = line["conversation_id"]
-            character_1 = line["character_id"]
-            return json
-            
+        result = {
+            "movie_id": movie_id,
+            "title": movie.title,
+            "top_characters": top_chars[0:5],
+        }
+        return result
+
     raise HTTPException(status_code=404, detail="movie not found.")
 
 
@@ -75,15 +64,12 @@ def list_movies(
     * `year`: The year the movie was released.
     * `imdb_rating`: The IMDB rating of the movie.
     * `imdb_votes`: The number of IMDB votes for the movie.
-
     You can filter for movies whose titles contain a string by using the
     `name` query parameter.
-
     You can also sort the results by using the `sort` query parameter:
     * `movie_title` - Sort by movie title alphabetically.
     * `year` - Sort by year of release, earliest to latest.
     * `rating` - Sort by rating, highest to lowest.
-
     The `limit` and `offset` query
     parameters are used for pagination. The `limit` query parameter specifies the
     maximum number of results to return. The `offset` query parameter specifies the
