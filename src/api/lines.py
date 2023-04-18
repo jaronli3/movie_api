4b47c39ec3ed6bbee0df2a7a93ff4fb4d199bad5
Jaron Li <2002jaron@gmail.com>
2023-04-17 20:59:24 -0700

    started lines

--- src/api/lines.py
+++ src/api/lines.py
@@ -10,20 +10,66 @@ router = APIRouter()
 @router.get("/lines/{line_id}", tags=["lines"])
 def get_line(line_id: int):
     """
-    This endpoint returns a single movie by its identifier. For each movie it returns:
-    * `movie_id`: the internal id of the movie.
-    * `title`: The title of the movie.
-    * `top_characters`: A list of characters that are in the movie. The characters
-      are ordered by the number of lines they have in the movie. The top five
-      characters are listed.
+    This endpoint returns a single line by its identifier. For each line it returns:
+    * `line_id`: the internal id of the line.
+    * `line_text`: The text of the line.
+    * 'movie': The movie the line came from.
+    * `characters_involved`: A list of characters that were involved in that conversation. 
+
     Each character is represented by a dictionary with the following keys:
     * `character_id`: the internal id of the character.
     * `character`: The name of the character.
-    * `num_lines`: The number of lines the character has in the movie.
+    * `gender`: The gender of the character.
+    * 'age': The age of the character.
     """
+
     line = db.lines.get(line_id)
     if line:
+        movie_id = None
+        char1 = None
+        char2 = None
+        convo_id = None
         json = {"line_id": line.id, "line_text": line.line_text}
+        movie_id = line.movie_id
+        char1 = line.character_id
+        convo_id = line.conv_id
+        for movie in db.movies:
+            if movie["movie_id"] == movie_id:
+                json["movie"] = movie["title"]
+                break
+
+        for convo in db.conversations:
+            if convo_id == convo["id"]:
+                if convo["character1_id"] == char1:
+                    char2 = convo["character2_id"]
+                elif convo["character2_id"] == char1:
+                    char2 = convo["character1_id"]
+                break
+                
+        lst = []
+        for char in db.characters:
+            char_json = {}
+            if char["character_id"] == char1:
+                char_json["character_id"] = char["character_id"]
+                char_json["name"] = char["name"]
+                if character["gender"] == "":
+                    char_json["gender"] = None
+                else:
+                    char_json["gender"] = char["gender"]
+                char_json["age"] = int(char["age"])
+                lst.append(char_json)
+            elif char["character_id"] == char2:
+                char_json["character_id"] = char["character_id"]
+                char_json["name"] = char["name"]
+                if character["gender"] == "":
+                    char_json["gender"] = None
+                else:
+                    char_json["gender"] = char["gender"]
+                char_json["age"] = int(char["age"])
+                lst.append(char_json)
+                
+        json["characters involved"] = lst
+
         return json
     
     raise HTTPException(status_code=404, detail="line not found.")
