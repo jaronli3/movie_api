28570664d8da0aed31a3d3294b7ae046d524784d
Jaron Li <2002jaron@gmail.com>
2023-04-19 17:57:36 -0700

    working second func

--- src/api/lines.py
+++ src/api/lines.py
@@ -69,15 +69,16 @@ def get_char_lines(character_id: int, limit: int = Query(50, ge=1, le=250), offs
     json = []
     char = db.characters.get(character_id)
     if char:
+        json.append(char.id)
+        json.append(char.name)
         for line_id in db.lines:
             new_line = db.lines.get(line_id)
             if new_line.c_id == char.id:
-                # json.append(new_line)
                 dictionary = {}
-                dictionary["character_id"] = char.id
-                dictionary["character_name"] = char.name
-            #     movie = db.movies.get(new_line.line_id)
-            #     dictionary["movie_title"] = movie.title
+                # dictionary["character_id"] = char.id
+                # dictionary["character_name"] = char.name
+                movie = db.movies.get(new_line.line_id)
+                dictionary["movie_title"] = movie.title
             #     dictionary["line"] = new_line.line_text
                 json.append(dictionary)
 
